45a73132258f0ea2e13b33f1a9de34afa4a0ad41
JaydaWatson <60038607+JaydaWatson@users.noreply.github.com>
2022-01-20 22:18:19 -0500

    Debug Update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,13 +16,13 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-    void OnMove(InputValue movementValue){
+    private void OnMove(InputValue movementValue){
         Vector2 movementVector = movementValue.Get<Vector2>();
         movementX = movementVector.x;
         movementY = movementVector.y;
     }
 
-    void FixedUpdate(){
+    private void FixedUpdate(){
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);
 
